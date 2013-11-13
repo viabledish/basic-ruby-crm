365e1fec978ae02e69c1a61c5b6efe3cc1981b32
viabledish <viable.dish@gmail.com>
2013-11-13 02:25:23 +0000

    added show command

--- application.rb
+++ application.rb
@@ -10,7 +10,9 @@ class Application
     @exit_cmd = 'exit'
     @new_cmd = 'new'
     @list_cmd = 'list'
-    @show_cmd = 'show'
+    @edit_name_cmd = 'edit name'
+    @edit_phone_cmd = 'edit phone'
+    @back_cmd = 'back'
 
   end
  
@@ -18,35 +20,24 @@ class Application
     begin 
         show_main_menu
         input = gets.chomp
-        case input
-          when (@new_cmd)
-            puts "Enter a first and last name"
-            full_name = gets.chomp
-            puts "Enter an email address"
-            email_address = gets.chomp
-
-            @contacts.each do |i|
-                if (i.email == email_address)
-                  puts "Email already exists!" 
-                else
-                  puts "#{email_address}" + " #{full_name}"
-                  new_card = Contact.new(full_name, email_address)
-                  @contacts << new_card
-                  puts 'Contact ' + new_card.to_s + ' created.'
-                end
-                break
-            end
-          
-          when (@list_cmd)
-            @contacts.each_with_index { |val, index| puts "#{index}: " + "#{@contacts[index]}"}
-          
-          when ('the'.to_s == 'the'.to_s)
-            puts "Remember, the index starts at 0!"
-
-            #card_index = gets.chomp.split[1]
-            #puts "#{@contacts}[card_index]"
+        if (input.start_with?"show")
+          show_cmd = input
         end
 
+        case input
+          when (@new_cmd)
+            create_new_card(@new_cmd)
+
+          when (@list_cmd)
+            list_cards
+          
+          when (show_cmd)
+            show_card(show_cmd)
+            input = gets.chomp
+            begin
+              edit_card
+            end while (input != @back_cmd)
+          end
     end while (input != @exit_cmd)
   end
   
@@ -57,6 +48,44 @@ class Application
     puts " list     - List all contacts"
     puts " show :id - Display contact details"
     print "> "
+  end
+
+  # Creates a new card
+  def create_new_card(new_command)
+    puts "Enter a first and last name"
+    full_name = gets.chomp
+    puts "Enter an email address"
+    email_address = gets.chomp
+
+    # check if there is an existing card
+    existing_contact = @contacts.detect {|c| c.email == email_address }
+    if existing_contact
+      puts "Contact already exists!"
+      return  
+    end
+
+    # create card
+    new_card = Contact.new(full_name, email_address)
+    @contacts << new_card
+    puts 'Contact ' + new_card.to_s + ' created.'
+  end
+
+  def list_cards
+    @contacts.each_with_index { |val, index| puts "#{index}: " + "#{@contacts[index]}"}
+  end
+
+  def show_card(show_command)
+    puts "Remember, the index starts at 0!"
+    card_index = (show_command.split(' '))[1]
+      if (@contacts[card_index.to_i] == nil)
+        puts "The card does not exist!"
+      else
+        puts "#{card_index}: " + "#{@contacts[card_index.to_i]}"
+      end
+  end
+
+  def edit_card
+    puts "edit!"
   end
  
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'pry-debugger'
+
 require_relative 'contact'
 require_relative 'application'
  
